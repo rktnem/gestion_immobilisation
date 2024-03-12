3be81d195083f22d9d032643902da3bcf218aab3
Nehemia <nehemiarakotondramanantsoa19@gmail.com>
2024-03-12 10:06:18 +0300

    test de dompdf

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/create', function () {
+    return view('create');
+})->name('create.show');
+
+Route::post('create', function () {
+    $pdf = Pdf::loadView('welcome');
+
+    return $pdf->download('invoice.pdf');
+})->name('create.send');
