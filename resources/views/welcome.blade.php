c86d4459183655a93f9dee5da6ec9166f1213b9d
Nehemia <nehemiarakotondramanantsoa19@gmail.com>
2024-03-12 15:24:26 +0300

    Correction du depot

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -831,7 +831,6 @@
 </head>
 
 <body class="antialiased">
-    <a href={{ route('create.show') }}>Aller</a>
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         @if (Route::has('login'))
