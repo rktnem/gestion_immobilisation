cd0e522141416ffa8adde07e123a37f33f9c39a7
Nehemia <nehemiarakotondramanantsoa19@gmail.com>
2024-03-22 13:41:33 +0300

    Developpement du fenetre d'affichage des matieres

--- resources/views/pages/displayImmo.blade.php
+++ resources/views/pages/displayImmo.blade.php
@@ -3,11 +3,40 @@
 @section('content')
 
 <section class="display-immobilisation">
-    <div class="breadcrumbs">
-        <a href="">Inventaire</a>
-        <a href="">
-            <span class="breadcrumbs-active">Liste des matieres</span>
-        </a>
+    <nav>
+        <div class="breadcrumbs">
+            <a href="">Inventaire</a>
+            <a href="">
+                <span class="breadcrumbs-active">Liste des matieres</span>
+            </a>
+        </div>
+        <div class="search-bar">
+            <input type="text" name="" id="" placeholder="Rechercher...">
+        </div>
+    </nav>
+    <div class="display-content">
+        <table>
+            <tr>
+                <th>Designation</th>
+                <th>Quantite</th>
+                <th>Unite</th>
+                <th>Prix</th>
+                <th>Prix total</th>
+                <th>Mode d'acquisition</th>
+                <th>Date d'acquisition</th>
+            </tr>
+            <tr>
+                <td>
+                    Imprimante 3D Acer Treva maxi, 1990*260 importe d'allemagne
+                </td>
+                <td>Test lesy ty eee</td>
+                <td>Test lesy ty eee</td>
+                <td>Test lesy ty eee</td>
+                <td>Test lesy ty eee</td>
+                <td>Test lesy ty eee</td>
+                <td>Test lesy ty eee</td>
+            </tr>
+        </table>
     </div>
 </section>
 
