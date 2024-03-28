fd9219a5a8df4a6934c36ab8cc6ea4b0eb98acec
Nehemia <nehemiarakotondramanantsoa19@gmail.com>
2024-03-28 10:12:17 +0300

    Finition de la partie pv de reception dans la section achat

--- resources/js/script.js
+++ resources/js/script.js
@@ -14,3 +14,27 @@ show.on('click', () => {
     close.show();
     show.hide();
 })
+
+// Pour les balises form repetitive
+
+let number = $("#number");
+let form = "";
+let formLot = $('#form-lot');
+
+function formGroup(lot) {
+    return " <div class='form-group'> <label for=lot-"+lot+">Lot n°"+lot+"</label> <input type='text' name='lot-"+lot+"' id='' placeholder='Designation du lot n°"+lot+"...'><input type='number' name='quantite-"+lot+"' id='' style='width: 75px' placeholder='Qté...'></div>"
+}
+
+number.on("keypress", (e) => {
+    console.log(form);
+    if(e.key === "Enter") {
+        formLot.children().remove();
+        form = "<h4>Désignation des lots</h4>";
+        e.preventDefault();
+        for(let i = 0; i < e.target.value; i++) {
+            form += formGroup(i+1)
+        }
+        formLot.append(form);
+    }
+    form = "";
+})
--- resources/views/pages/type/achat.blade.php
+++ resources/views/pages/type/achat.blade.php
@@ -17,7 +17,33 @@
             </li>
         </ul>
         <div class="save-part">
-
+            <h3>Renseigner le pv de reception</h3>
+            <form action="">
+                @csrf
+                <div class="form">
+                    <div class="form-general">
+                        <div class="form-group">
+                            <label for="ref">Réference du DAO</label>
+                            <input type="text" name="ref" id="" style="max-width: 100px;" placeholder="Réference...">
+                        </div>
+                        <div class="form-group">
+                            <label for="objet">Objet</label>
+                            <input type="text" name="objet" id="" style="min-width: 300px;"
+                                placeholder="Titre de l'objet...">
+                        </div>
+                        <div class="form-group">
+                            <label for="number">Nombre de lot</label>
+                            <input type="number" name="number" id="number" style="max-width: 100px;"
+                                placeholder="Nombre de lot...">
+                        </div>
+                    </div>
+                    <div class="form-lot" id="form-lot">
+                        <h4>Désignation des lots</h4>
+                    </div>
+                </div>
+                <div class="form-button">
+                    <input type="submit" value="VALIDER">
+                </div>
+            </form>
         </div>
-    </div>
 </section>
