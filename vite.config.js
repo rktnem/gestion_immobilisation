dc68f73e43beb7932ddd5cacdffd802a54d471de
Nehemia <nehemiarakotondramanantsoa19@gmail.com>
2024-04-08 10:54:50 +0300

    Creation des models principales

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-	server: {
-		hmr: {
-			host: "192.168.137.6"
-		}
-	},
+	// server: {
+	// 	hmr: {
+	// 		host: "192.168.137.6"
+	// 	}
+	// },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -14,3 +14,4 @@ export default defineConfig({
         }),
     ],
 });
+j
